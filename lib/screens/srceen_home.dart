29785f500d452c04a5a9d233b01f0fc118875039
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-13 11:53:08 +0900

    code : add common route

--- lib/screens/srceen_home.dart
+++ lib/screens/srceen_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/screens/srceen_first.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -8,6 +9,18 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("라우트 관리 홈"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(child: Text("일반적인 라우트"), onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => FirstPage()));
+            }),
+            // ElevatedButton(onPressed: () {}, child: Text("일반적인 라우트")),
+            // ElevatedButton(onPressed: () {}, child: Text("일반적인 라우트")),
+          ],
+        ),
       ),
     );
   }
